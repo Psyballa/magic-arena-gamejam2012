44b97a080265715f0fe3b7583d4ece8848248c23
logancf <logancf@829f7535-af00-5bbb-c621-0e7cfba0846b>
2012-01-28 08:12:55 +0000

    Attacks.cs stub

--- trunk/2012GameJam/2012GameJam/Attacks.cs
+++ trunk/2012GameJam/2012GameJam/Attacks.cs
@@ -7,5 +7,11 @@ namespace _2012Gamejam
 {
     class Attacks
     {
+        //make a body and fixture, in given direction w/ particle emitter, trigger on hit player
+        public Attacks()
+        {
+
+        }
+
     }
 }
