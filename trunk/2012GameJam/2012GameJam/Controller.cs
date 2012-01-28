1130028481510595e5f1d8d05f488dd5c405e90f
Psyballa <Psyballa@829f7535-af00-5bbb-c621-0e7cfba0846b>
2012-01-28 06:57:34 +0000

    Controller class.

---
+++ trunk/2012GameJam/2012GameJam/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+
+/// <summary>
+/// Summary description for Controller class.
+/// </summary>
+public class Controller
+{
+    GamePadState playerOne = GamePad.GetState(PlayerIndex.One);
+    GamePadState playerTwo = GamePad.GetState(PlayerIndex.Two);
+    GamePadState playerThree = GamePad.GetState(PlayerIndex.Three);
+    GamePadState playerFour = GamePad.GetState(PlayerIndex.Four);
+
+
+    /// <summary>
+    /// Constructor for controller class. 
+    /// Each controller takes in a Player
+    /// </summary>
+	public Controller(GamePadState gamePadState, Button button)
+	{
+        this.gamePadState = gamePadState;
+        this.button = button;
+	}
+}
