70a96e9842d8bef5b673595f514e080887cfe672
ryan.theultimatemuffin <ryan.theultimatemuffin@829f7535-af00-5bbb-c621-0e7cfba0846b>
2012-01-28 07:13:20 +0000

    started on player, mostly just a constructor at this point.

--- 2012GameJam/2012GameJam/Player.cs
+++ 2012GameJam/2012GameJam/Player.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Collision;
 
 namespace _2012Gamejam
 {
     class Player
     {
+        int health;
+        Body playerBody;
+        Fixture playerFix;
+        CircleShape playerBodyShape;
+        Texture2D playerTex;
+        int playerNumber;
+
+
+        public Player(int playerNum, World gameWorld)
+        {
+            health = 100;
+            playerBody.BodyType = BodyType.Dynamic;
+            playerBodyShape = new CircleShape(1.0f, 1.0f);
+            playerFix = playerBody.CreateFixture(playerBodyShape);
+            gameWorld.AddBody(playerBody);
+            playerNumber = playerNum;
+            ////////////////////////////////////////////////////////// playerTex = ? also starting coords all based off player #
+        }
+
+        public void update(){
+
+
+
+        
     }
 }
