3d14466929828dc31c18a401d6d074f897eca6ce
logancf <logancf@829f7535-af00-5bbb-c621-0e7cfba0846b>
2012-01-29 15:43:39 +0000

    fleshed out the sound manager a bit.

--- Everyone use this/WindowsGame1/WindowsGame1/SoundManager.cs
+++ Everyone use this/WindowsGame1/WindowsGame1/SoundManager.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Audio
 
 namespace WindowsGame1
 {
     class SoundManager
     {
         public static SoundManager s;
+        bool playingVoice;
+        SoundEffectInstance currVoice;
+
+        // put sound variables here (make them public)
+
         private SoundManager()
         {
             //load sounds in here
         }
+
+        // accepts a sound as an argument
+        public void play ()
+        {
+
+        }
+
+        public void playVoice( SoundEffect inSound)
+        {
+            if (!playingVoice)
+            {
+                currVoice = inSound.CreateInstance();
+                playingVoice = true;
+            }
+        }
+
+        public void update()
+        {
+            if(currVoice != null && currVoice.State != SoundState.Playing)
+            {
+                playingVoice = false;
+                //currVoice.Dispose();  // do I need to do this?
+            }
+        }
+
     }
 }
