5c6ba4061270ed4a6455ea3dc93d143957e4103f
logancf <logancf@829f7535-af00-5bbb-c621-0e7cfba0846b>
2012-01-29 10:23:25 +0000

    Added a stub for sound manager that implements the singleton pattern

---
+++ Everyone use this/WindowsGame1/WindowsGame1/SoundManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1
+{
+    class SoundManager
+    {
+        public static SoundManager s;
+        private SoundManager()
+        {
+            //load sounds in here
+        }
+    }
+}
